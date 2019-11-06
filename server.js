d03e7d2be38c7ea54f68172b1e95f3ff99b4dd4a
Gerard Hynes <gkhynes@gmail.com>
2019-11-06 20:05:30 +0000

    Simulate login process

--- server.js
+++ server.js
@@ -6,8 +6,26 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express");
+app.post("/contact", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).send("Name is required");
+  }
+
+  // DATABASE STUFF
+  res.status(201).send(`Thank you ${req.body.name}`);
+});
+
+// Simulate login process
+app.post("/login", (req, res) => {
+  if (!req.header("x-auth-token")) {
+    return res.status(400).send("No token");
+  }
+
+  if (req.header("x-auth-token") !== "123456") {
+    return res.status(401).send("Not authorized");
+  }
+
+  res.send("Logged in");
 });
 
 app.listen(5000, () => console.log("Server started on port 5000"));
